399a400eccccb38e94afc0b8a1ac8784b29d20a3
A-Kija <arvydas.kijakauskas@gmail.com>
2024-11-12 13:35:10 +0200

    016

--- mini-shop/app.js
+++ mini-shop/app.js
@@ -1,33 +1,35 @@
-let C = [
-    {
-        id: 1,
-        img: './images/kede.jpg',
-        title: 'Kedė',
-        price: 50,
-        quantity: 4
-    },
-    {
-        id: 2,
-        img: './images/stalas.jpg',
-        title: 'Stalas',
-        price: 100,
-        quantity: 2
-    },
-    {
-        id: 3,
-        img: './images/sofa.jpg',
-        title: 'Sofa',
-        price: 200,
-        quantity: 3
-    },
-    {
-        id: 4,
-        img: './images/komoda.jpg',
-        title: 'Komoda',
-        price: 150,
-        quantity: 1
-    }
-];
+// let C = [
+//     {
+//         id: 1,
+//         img: './images/kede.jpg',
+//         title: 'Kedė',
+//         price: 50,
+//         quantity: 4
+//     },
+//     {
+//         id: 2,
+//         img: './images/stalas.jpg',
+//         title: 'Stalas',
+//         price: 100,
+//         quantity: 2
+//     },
+//     {
+//         id: 3,
+//         img: './images/sofa.jpg',
+//         title: 'Sofa',
+//         price: 200,
+//         quantity: 3
+//     },
+//     {
+//         id: 4,
+//         img: './images/komoda.jpg',
+//         title: 'Komoda',
+//         price: 150,
+//         quantity: 1
+//     }
+// ];
+
+let C = [];
 
 
 const cartIcon = document.querySelector('[data-cart-icon]');
@@ -42,6 +44,55 @@ cartIcon.addEventListener('click', _ => {
         cartList.style.maxHeight = '0';
     }
 });
+
+const showMessage = _ => {
+    const message = document.querySelector('[data-show]');
+    message.dataset.show = true;
+    setTimeout(_ => {
+        message.dataset.show = false;
+    }, 1500);
+}
+
+
+const productsAction = _ => {
+
+    const products = document.querySelectorAll('[data-product]');
+
+    products.forEach(product => {
+        const button = product.querySelector('button');
+        const img = product.querySelector('img').src;
+        const input = product.querySelector('input');
+
+        button.addEventListener('click', _ => {
+            const id = parseInt(button.dataset.id);
+            const name = button.dataset.name;
+            const price = parseFloat(button.dataset.price);
+            const quantity = parseInt(input.value);
+
+            C.push(
+                {
+                    id,
+                    img,
+                    title: name,
+                    price,
+                    quantity
+                }
+            );
+
+            showMessage();
+            cartRender();
+            addEvents();
+            const cartList = document.querySelector('[data-cart-list]');
+            cartList.dataset.open = 'close';
+            cartList.style.maxHeight = '0';
+
+
+        });
+    });
+
+
+
+}
 
 
 
@@ -70,19 +121,20 @@ const cartRender = _ => {
 
 const addEvents = _ => {
     document.querySelectorAll('[data-cart-list] ul li:not([data-empty])')
-    .forEach(li => {
-        const button = li.querySelector('button');
-        button.addEventListener('click', _ => {
-            const id = button.dataset.id;
-            C = C.filter(item => item.id !== parseInt(id));
-            cartRender();
-            addEvents();
+        .forEach(li => {
+            const button = li.querySelector('button');
+            button.addEventListener('click', _ => {
+                const id = button.dataset.id;
+                C = C.filter(item => item.id !== parseInt(id));
+                cartRender();
+                addEvents();
+            });
         });
-    });
 }
 
 cartRender();
 addEvents();
+productsAction();
 
 
 
