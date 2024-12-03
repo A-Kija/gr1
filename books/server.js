7ff331e73c0ef0d92ab60e4f2d576a4a57f19ba4
A-Kija <arvydas.kijakauskas@gmail.com>
2024-12-03 13:35:01 +0200

    028

--- books/server.js
+++ books/server.js
@@ -5,15 +5,28 @@ const fs = require('node:fs');
 const app = express();
 const port = 80;
 
+const domain = 'http://books.final/';
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const top = fs.readFileSync('./html/top.html', 'utf8');
+const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+
+
+
 
 // ROUTES
 
 app.get('/create', (req, res) => {
-  const file = fs.readFileSync('./html/create.html', 'utf8');
+  const file = top + fs.readFileSync('./html/create.html', 'utf8') + bottom;
   const template = handlebars.compile(file);
-  const data = { manoKintamasis: 'Labas' };
+  const data = { 
+    manoKintamasis: 'Labas, Bebrai!',
+    pageTitle: 'Nauja knyga',
+    domain: domain
+   };
   const html = template(data);
   res.send(html);
 
