a18e13a49c34ebb18ae496792ec3c2d90ff9ad46
A-Kija <arvydas.kijakauskas@gmail.com>
2025-02-13 12:31:39 +0200

    065

--- breddit/seeder/index.js
+++ breddit/seeder/index.js
@@ -1,5 +1,86 @@
 import { users } from './users.js';
+import { posts } from './posts.js';
 import mysql from 'mysql';
+import { faker } from '@faker-js/faker';
+
+function rand(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+const usersCount = users.length;
+
+// add author_id to posts
+posts.forEach(post => {
+    post.author_id = users[rand(0, usersCount - 1)].id;
+});
+
+// add likes array to posts
+posts.forEach(post => {
+    post.likes = { l: [], d: [] };
+    const likesCount = rand(0, usersCount);
+    for (let i = 0; i < likesCount; i++) {
+        let likeId;
+        do {
+            likeId = users[rand(0, usersCount - 1)].id;
+        } while (post.likes.l.includes(likeId));
+        post.likes.l.push(likeId);
+    }
+    users.forEach(user => {
+        if (!post.likes.l.includes(user.id)) {
+          rand(0, 2) || (post.likes.d.push(user.id));
+        }
+    });
+});
+
+// posts likes to JSON
+posts.forEach(post => {
+    post.likes = JSON.stringify(post.likes);
+});
+
+
+let lastCommentId = 1;
+const comments = [];
+const postCommentsCount = new Map();
+
+// comments to posts and comments
+posts.forEach(post => {
+    postCommentsCount.set(post.id, 0);
+    if (rand(0, 8)) {
+        const commentsCount = rand(0, 10);
+        for (let i = 0; i < commentsCount; i++) {
+            const comment = {
+                id: lastCommentId++,
+                post_id: post.id,
+                comment_id: null,
+                author_id: users[rand(0, usersCount - 1)].id,
+                content: faker.lorem.paragraph(),
+            };
+            comments.push(comment);
+            postCommentsCount.set(post.id, postCommentsCount.get(post.id) + 1);
+            if (rand(0, 8)) {
+                const repliesCount = rand(0, 5);
+                for (let j = 0; j < repliesCount; j++) {
+                    const reply = {
+                        id: lastCommentId++,
+                        comment_id: comment.id,
+                        post_id: null,
+                        author_id: users[rand(0, usersCount - 1)].id,
+                        content: faker.lorem.paragraph(),
+                    };
+                    comments.push(reply);
+                    postCommentsCount.set(post.id, postCommentsCount.get(post.id) + 1);
+                }
+            }
+        }
+    }
+});
+
+// add comments count to posts
+posts.forEach(post => {
+    post.comments = postCommentsCount.get(post.id);
+});
+
 
 
 let sql;
@@ -49,9 +130,37 @@ con.query(sql, [users.map(user => [user.id, user.name, user.avatar, user.role, u
 });
 
 
+sql = `
+    CREATE TABLE posts (
+    id int(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    author_id int(10) UNSIGNED NOT NULL,
+    content text NOT NULL,
+    image_url varchar(100) NOT NULL,
+    likes text NOT NULL,
+    title varchar(255) NOT NULL,
+    date date NOT NULL,
+    comments int(10) UNSIGNED NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ `;
+
+con.query(sql, (err) => {
+    if (err) throw err;
+    console.log('Lentelė posts sukurta!');
+});
+
+sql = `
+    INSERT INTO posts
+    (id, author_id, content, image_url, likes, title, date, comments)
+    VALUES ?
+`;
+
+con.query(sql, [posts.map(post => [post.id, post.author_id, post.content, post.img_url, post.likes, post.title, post.date, post.comments])], (err) => {
+    if (err) throw err;
+    console.log('Duomenys įterpti į lentelę posts!');
+});
 
 
-
+console.log(faker.word.words({ count: { min: 5, max: 100 } }));
 
 
 con.end(err => {
